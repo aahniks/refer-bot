4f3bf4a52959dcee687ce900e29d8d534fc3d3be
aahnik <daw@aahnik.dev>
2021-06-18 16:05:37 +0530

    add conv flow & validation to set wallet  fix #4

--- refer_bot/handlers/user.py
+++ refer_bot/handlers/user.py
@@ -2,11 +2,12 @@
 
 import logging
 
-from telethon import Button, events
+from telethon import Button, TelegramClient, events
 
 from refer_bot import conf, messages
 from refer_bot import storage as st
 from refer_bot.handlers._utils import build_keyboard, join_protect
+from refer_bot.handlers.const import main_kbd
 from refer_bot.types import EventLike
 
 
@@ -25,7 +26,7 @@ async def refresh_button_click_handler(event: events.CallbackQuery.Event, user):
     if user.joined:
         await event.edit(
             "Thank you for joining the channels!",
-            buttons=build_keyboard(messages.user_kbd),
+            buttons=build_keyboard(messages.user_kbd_matrix),
         )
 
 
@@ -43,7 +44,9 @@ async def profile_handler(event: EventLike, user):
             joined=user.joined,
             coins=user.coins,
             ref_count=len(user.referals),
-            wallet=user.wallet or "Not set",
+            wallet=(user.wallet + "(" + str(user.phone) + ")")
+            if user.wallet
+            else "Not set",
         ),
         buttons=wallet_btn,
     )
@@ -57,11 +60,75 @@ async def wallet_button_click_handler(event: events.CallbackQuery.Event, user):
 
     await event.answer("Click on wallet button recieved")
 
-    if not user.wallet:
-        await event.edit("This feature is coming soon!")
-        # conversation to get wallet info
-    else:
+    if user.wallet:
         await event.edit("Your wallet is already set.")
+    await event.edit("You must be INDIAN to get cash!")
+    client: TelegramClient = event.client
+    async with client.conversation(event.sender_id) as conv:
+        message = await conv.send_message(
+            "Please Choose your wallet",
+            buttons=build_keyboard([messages.wallet_options]),
+        )
+        wallet_reply = await conv.get_response(message)
+        wallet = wallet_reply.text
+        if wallet in messages.wallet_options:
+            diff_no = "Send a different number"
+            only_ind = "Only Indian Numbers allowed"
+            ask_phone = await conv.send_message(
+                f"Send your 10 digit {wallet} number",
+                buttons=[
+                    [
+                        Button.request_phone(
+                            f"Send my Telegram number", resize=True, single_use=True
+                        )
+                    ],
+                    [Button.text(diff_no)],
+                ],
+            )
+            phn_reply = await conv.get_response(ask_phone)
+
+            if phn_reply.contact:
+                phone: str = phn_reply.contact.phone_number
+                if not phone.startswith("91"):
+                    await conv.send_message(
+                        messages.wallet_set_failed.format(reason=only_ind),
+                        buttons=main_kbd,
+                    )
+                phone = int(phone[2:])
+            elif phn_reply.text == diff_no:
+
+                ask_phn_manual = await conv.send_message(
+                    f"Type and send your {wallet} number.\n\
+                    \n(Must be 10 Digit Indian Phone number)",
+                    buttons=Button.clear(),
+                )
+                reply_phn = await conv.get_response(ask_phn_manual)
+                phone: str = reply_phn.text
+
+                try:
+                    if phone.startswith("91") and len(phone) == 12:
+                        phone = int(phone[2:])
+                    elif len(phone) == 10:
+                        phone = int(phone)
+                    else:
+                        raise ValueError
+                except:
+                    await conv.send_message(
+                        messages.wallet_set_failed.format(
+                            reason=f"Invalid phone number format. Give 10 digit number. {only_ind}"
+                        ),
+                        buttons=main_kbd,
+                    )
+                    conv.cancel()
+
+        if phone:
+            user.phone = phone
+            user.wallet = wallet
+            await st.engine.save(user)
+            await conv.send_message(
+                f"Your {wallet} number is set as {phone} 🎉",
+                buttons=main_kbd,
+            )
 
 
 @events.register(events.NewMessage(pattern=messages.my_referals_btn))
@@ -72,7 +139,6 @@ async def my_referals_handler(event: EventLike, user: st.Person):
         f"No of Sucessful referals **{ref_count}**\n\
         \nThat means {ref_count} people started the bot from the link you shared."
     )
-    # TODO: paginated list of referals username str
 
 
 @events.register(events.NewMessage(pattern=messages.get_link_btn))
@@ -86,7 +152,7 @@ async def get_link_handler(event: EventLike, user):
 @join_protect
 async def get_cash_handler(event: EventLike, user: st.Person):
     min_limit = 100
-    if user.coins < 100:
+    if user.coins < min_limit:
         msg = messages.not_sufficient_coins
     else:
         msg = messages.get_cash_text
