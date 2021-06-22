d08374f0e96119273a0f0e7e1b98be840e2650c2
aahnik <daw@aahnik.dev>
2021-06-22 23:14:49 +0530

    add user phone no verification
    to allow only indian users

--- refer_bot/handlers/start.py
+++ refer_bot/handlers/start.py
@@ -1,12 +1,13 @@
 import logging
 
-from telethon import TelegramClient, events
+from telethon import Button, TelegramClient, events
 
 from refer_bot import messages
 from refer_bot import storage as st
 from refer_bot.handlers._utils import build_keyboard, get_args
 from refer_bot.handlers.const import main_kbd
 from refer_bot.types import EventLike
+from refer_bot.utils import get_id
 
 
 @events.register(events.NewMessage(pattern="/start"))
@@ -35,7 +36,44 @@ async def start_handler(event: EventLike):
         logging.info("This user is already stored, and data cant be changed.")
     else:
         await event.respond(messages.new_user_start)
-        await st.engine.save(this_user)
+        fetched_user = await st.engine.save(this_user)
+    this_user = fetched_user
+
+    if not this_user.verified:
+        client: TelegramClient = event.client
+        async with client.conversation(event.sender_id) as conv:
+            ask_phn = await conv.send_message(
+                "You need to do phone number verification to start using this bot. Click the verify button to proceed.",
+                buttons=Button.request_phone(
+                    "Verify my number", resize=True, single_use=True
+                ),
+            )
+            phn_reply = await conv.get_response(ask_phn)
+
+            if not phn_reply.contact:
+                await conv.send_message(messages.veri_failed, buttons=Button.clear())
+                raise events.StopPropagation
+
+            phone: str = phn_reply.contact.phone_number
+            contact_id: int = phn_reply.contact.user_id
+
+            if not (phone.startswith("91") or phone.startswith("+91")):
+                await st.engine.save(this_user)
+                await conv.send_message(
+                    "❗ You are not an INDIAN User.Sorry, this bot is only for INDIANs",
+                    buttons=Button.clear(),
+                )
+                raise events.StopPropagation
+
+            if not contact_id == event.sender_id:
+                await conv.send_message(messages.veri_failed, buttons=Button.clear())
+                raise events.StopPropagation
+
+            this_user.verified = True
+            await conv.send_message(
+                "✅ You are verified and now you can start using the bot!"
+            )
+            await st.engine.save(this_user)
 
     await event.respond(
         "Click on any keyboard button to proceed!",
